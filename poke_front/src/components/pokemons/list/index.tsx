fef63ed15e4208745cc3906c86236a28a47f4db2
manuelmartin-developer <mmartindj@gmail.com>
2023-12-13 08:16:09 +0100

    fix: fix some styles

--- poke_front/src/components/pokemons/list/index.tsx
+++ poke_front/src/components/pokemons/list/index.tsx
@@ -5,21 +5,14 @@ import PokemonItem from "../item";
 
 const PokemonList: React.FC<{ pokemons: Pokemon[] }> = ({ pokemons }) => {
   return (
-    <>
-      <ul className={styles.list}>
-        <AnimatePresence mode="sync">
-          {pokemons &&
-            pokemons.map((pokemon: Pokemon) => (
-              <PokemonItem key={pokemon.id} pokemon={pokemon} />
-            ))}
-        </AnimatePresence>
-      </ul>
-      {/* {searchInput && data.length === 0 && (
-        <div className={styles.list__no_results}>
-          <p className={styles.list__no_results__text}>No results found</p>
-        </div>
-      )} */}
-    </>
+    <ul className={styles.list}>
+      <AnimatePresence mode="sync">
+        {pokemons &&
+          pokemons.map((pokemon: Pokemon) => (
+            <PokemonItem key={pokemon.id} pokemon={pokemon} />
+          ))}
+      </AnimatePresence>
+    </ul>
   );
 };
 
